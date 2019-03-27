7cbf8d33f91b3c2a4c7af9cc784305598385bf22
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2019-03-27 15:54:40 +0100

    just formatting

--- src/main/java/life/qbic/portal/portlet/ProjectFilterGenerator.java
+++ src/main/java/life/qbic/portal/portlet/ProjectFilterGenerator.java
@@ -1,5 +1,8 @@
 package life.qbic.portal.portlet;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.tepi.filtertable.FilterGenerator;
 
 import com.vaadin.data.Container.Filter;
@@ -13,60 +16,54 @@ public class ProjectFilterGenerator implements FilterGenerator {
 
   @Override
   public Filter generateFilter(Object propertyId, Object value) {
-      if ("id".equals(propertyId)) {
-          /* Create an 'equals' filter for the ID field */
-          if (value != null && value instanceof String) {
-              try {
-                  return new Compare.Equal(propertyId,
-                          Integer.parseInt((String) value));
-              } catch (NumberFormatException ignored) {
-                  // If no integer was entered, just generate default filter
-              }
-          }
-      } else if ("checked".equals(propertyId)) {
-          if (value != null && value instanceof Boolean) {
-              if (Boolean.TRUE.equals(value)) {
-                  return new Compare.Equal(propertyId, value);
-              } else {
-                  return new Or(new Compare.Equal(propertyId, true),
-                          new Compare.Equal(propertyId, false));
-              }
-          }
+    if ("id".equals(propertyId)) {
+      /* Create an 'equals' filter for the ID field */
+      if (value != null && value instanceof String) {
+        try {
+          return new Compare.Equal(propertyId, Integer.parseInt((String) value));
+        } catch (NumberFormatException ignored) {
+          // If no integer was entered, just generate default filter
+        }
       }
-      // For other properties, use the default filter
-      return null;
+    } else if ("checked".equals(propertyId)) {
+      if (value != null && value instanceof Boolean) {
+        if (Boolean.TRUE.equals(value)) {
+          return new Compare.Equal(propertyId, value);
+        } else {
+          return new Or(new Compare.Equal(propertyId, true), new Compare.Equal(propertyId, false));
+        }
+      }
+    }
+    // For other properties, use the default filter
+    return null;
   }
 
   @Override
   public Filter generateFilter(Object propertyId, Field<?> originatingField) {
-      // Use the default filter
-      return null;
+    // Use the default filter
+    return null;
   }
 
   @Override
   public AbstractField<?> getCustomFilterComponent(Object propertyId) {
-      // removed custom filter component for id
-      if ("checked".equals(propertyId)) {
-          CheckBox box = new CheckBox();
-          return box;
-      }
-      return null;
+    // removed custom filter component for id
+    if ("checked".equals(propertyId)) {
+      CheckBox box = new CheckBox();
+      return box;
+    }
+    return null;
   }
 
   @Override
-  public void filterRemoved(Object propertyId) {
-  }
+  public void filterRemoved(Object propertyId) {}
 
   @Override
-  public void filterAdded(Object propertyId,
-          Class<? extends Filter> filterType, Object value) {
-  }
+  public void filterAdded(Object propertyId, Class<? extends Filter> filterType, Object value) {}
 
   @Override
-  public Filter filterGeneratorFailed(Exception reason, Object propertyId,
-          Object value) {
-      /* Return null -> Does not add any filter on failure */
-      return null;
-}
+  public Filter filterGeneratorFailed(Exception reason, Object propertyId, Object value) {
+    /* Return null -> Does not add any filter on failure */
+    return null;
+  }
 
 }
