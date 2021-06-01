9e96398762da43bb36849ad0f781a92bee12501f
wow-such-code <andreas.friedrich@uni-tuebingen.de>
2021-06-01 17:38:02 +0200

    remove unnecessary tabs, use new database structure

--- src/main/java/life/qbic/userdb/views/ProjectView.java
+++ src/main/java/life/qbic/userdb/views/ProjectView.java
@@ -41,8 +41,6 @@ import com.vaadin.ui.Table;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
-import ch.systemsx.cisd.common.concurrent.TimerUtilities;
-import life.qbic.datamodel.identifiers.SampleCodeFunctions;
 import life.qbic.datamodel.persons.Affiliation;
 import life.qbic.datamodel.persons.CollaboratorWithResponsibility;
 import life.qbic.datamodel.persons.Person;
@@ -50,7 +48,6 @@ import life.qbic.datamodel.projects.ProjectInfo;
 import life.qbic.portal.Styles;
 import life.qbic.portal.portlet.ProjectFilterDecorator;
 import life.qbic.portal.portlet.ProjectFilterGenerator;
-import life.qbic.portal.utils.PortalUtils;
 import life.qbic.utils.TimeUtils;
 
 public class ProjectView extends VerticalLayout {
